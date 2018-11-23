507aec5cced07e7300bc1473b84eb101ce408354
Steven Salka <stevensalka925@gmail.com>
2018-11-23 10:50:17 -0800

    add build script

---
+++ scripts/build.sh
@@ -0,0 +1,10 @@
+if [[ `basename $PWD` == 'lib' || `basename $PWD` != 'common' ]]; then
+  echo 'Only run build in root directory'
+  exit 1
+fi
+
+echo 'Building project...'
+
+rm -rf lib
+tsc
+cp package.json lib
