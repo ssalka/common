37b5a7af31f7b088da3fd894c3b5d6d85d24b931
Steven Salka <stevensalka925@gmail.com>
2019-07-05 23:34:52 -0700

    v0.2.7
    Export fix

--- src/types/Populated.ts
+++ src/types/Populated.ts
@@ -1,7 +1,9 @@
 import ID from './ID';
 import Omit from './Omit';
 
-export type Populated<T extends {}, K extends keyof T = keyof T>
+type Populated<T extends {}, K extends keyof T = keyof T>
   = Omit<T, K> & {
     [k in K]: Exclude<T[k], ID>
   };
+
+export default Populated;
