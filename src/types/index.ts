a591894b990b22821a74055cbffbca97bc2cd0d9
Steven Salka <stevensalka925@gmail.com>
2019-07-05 23:04:11 -0700

    add branded ID type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export { default as ITimestamped } from './ITimestamped';
 export { default as IVersioned } from './IVersioned';
+export { default as ID } from './ID';
 export { default as Omit } from './Omit';
