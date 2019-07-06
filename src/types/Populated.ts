81ea7941bb412d436057e8eee622bd6476911060
Steven Salka <stevensalka925@gmail.com>
2019-07-05 23:12:32 -0700

    build fix

--- src/types/Populated.ts
+++ src/types/Populated.ts
@@ -1,9 +1,7 @@
 import ID from './ID';
 import Omit from './Omit';
 
-type WithoutId<T extends ID> = Exclude<T, ID>;
-
 export type Populated<T extends {}, K extends keyof T = keyof T>
   = Omit<T, K> & {
-    [k in K]: WithoutId<T[k]>
+    [k in K]: Exclude<T[k], ID>
   };
